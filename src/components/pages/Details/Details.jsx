9f5484dfd624fcebfb8e0197d8a30a97ca6ee156
Matthew7991 <Matthew7991@gmx.de>
2023-08-21 11:16:56 +0200

    details basic logic

--- src/components/pages/Details/Details.jsx
+++ src/components/pages/Details/Details.jsx
@@ -1,25 +1,131 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+// http://localhost:5173/details/52772
 
 function Details() {
+  const [meal, setMeal] = useState({})
+  const [showInstructions, setShowInstructions] = useState(false)
+
+  const mealId = useParams().mealID
+
+  const handleToggleDisplay = () => {
+    setShowInstructions(!showInstructions)
+  }
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("fetching failed")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        // console.log(data.meals[0])
+        setMeal(data.meals[0])
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
+  }, [])
+
+  // if (!meal) {
+  //   return <h1>loading ...</h1>
+  // }
+
+  // console.log(meal)
+
   return (
     <main>
       <article>
         <img
-          src=""
+          src={meal.strMealThumb}
           alt=""
         />
         <div>
-          <h1></h1>
-          <p></p>
-          <p></p>
+          <h1>{meal.strMeal}</h1>
+          <p>{meal.strCategory}</p>
+          <p>{meal.strArea}</p>
           <div>
-            <button>Ingredients</button>
-            <button>Instructions</button>
+            <button onClick={handleToggleDisplay}>Ingredients</button>
+            <button onClick={handleToggleDisplay}>Instructions</button>
           </div>
-          <div>
-            <h2>Ingredients</h2>
-            <p></p>
-          </div>
+          {showInstructions || (
+            <div>
+              <h2>Ingredients</h2>
+              {meal.strIngredient1 && (
+                <p>{`${meal.strMeasure1} ${meal.strIngredient1}`}</p>
+              )}
+              {meal.strIngredient2 && (
+                <p>{`${meal.strMeasure2} ${meal.strIngredient2}`}</p>
+              )}
+              {meal.strIngredient3 && (
+                <p>{`${meal.strMeasure3} ${meal.strIngredient3}`}</p>
+              )}
+              {meal.strIngredient4 && (
+                <p>{`${meal.strMeasure4} ${meal.strIngredient4}`}</p>
+              )}
+              {meal.strIngredient5 && (
+                <p>{`${meal.strMeasure5} ${meal.strIngredient5}`}</p>
+              )}
+              {meal.strIngredient6 && (
+                <p>{`${meal.strMeasure6} ${meal.strIngredient6}`}</p>
+              )}
+              {meal.strIngredient7 && (
+                <p>{`${meal.strMeasure7} ${meal.strIngredient7}`}</p>
+              )}
+              {meal.strIngredient8 && (
+                <p>{`${meal.strMeasure8} ${meal.strIngredient8}`}</p>
+              )}
+              {meal.strIngredient9 && (
+                <p>{`${meal.strMeasure9} ${meal.strIngredient9}`}</p>
+              )}
+              {meal.strIngredient10 && (
+                <p>{`${meal.strMeasure10} ${meal.strIngredient10}`}</p>
+              )}
+              {meal.strIngredient11 && (
+                <p>{`${meal.strMeasure11} ${meal.strIngredient11}`}</p>
+              )}
+              {meal.strIngredient12 && (
+                <p>{`${meal.strMeasure12} ${meal.strIngredient12}`}</p>
+              )}
+              {meal.strIngredient13 && (
+                <p>{`${meal.strMeasure13} ${meal.strIngredient13}`}</p>
+              )}
+              {meal.strIngredient14 && (
+                <p>{`${meal.strMeasure14} ${meal.strIngredient14}`}</p>
+              )}
+              {meal.strIngredient15 && (
+                <p>{`${meal.strMeasure15} ${meal.strIngredient15}`}</p>
+              )}
+              {meal.strIngredient16 && (
+                <p>{`${meal.strMeasure16} ${meal.strIngredient16}`}</p>
+              )}
+              {meal.strIngredient17 && (
+                <p>{`${meal.strMeasure17} ${meal.strIngredient17}`}</p>
+              )}
+              {meal.strIngredient18 && (
+                <p>{`${meal.strMeasure18} ${meal.strIngredient18}`}</p>
+              )}
+              {meal.strIngredient19 && (
+                <p>{`${meal.strMeasure19} ${meal.strIngredient19}`}</p>
+              )}
+              {meal.strIngredient20 && (
+                <p>{`${meal.strMeasure20} ${meal.strIngredient20}`}</p>
+              )}
+            </div>
+          )}
+          {showInstructions && (
+            <div>
+              <h2>Instructions</h2>
+              <p>{meal.strInstructions}</p>
+              <a
+                href={meal.strYoutube}
+                target="_blank">
+                Video
+              </a>
+            </div>
+          )}
         </div>
       </article>
     </main>
