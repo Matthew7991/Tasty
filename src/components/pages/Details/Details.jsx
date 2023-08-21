66cd04bbfe1cac40688ac54863edb8ab0f35a97e
Matthew7991 <Matthew7991@gmx.de>
2023-08-21 14:02:10 +0200

    details desgin

--- src/components/pages/Details/Details.jsx
+++ src/components/pages/Details/Details.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import "./Details.scss"
 
 function Details() {
   const [meal, setMeal] = useState({})
@@ -37,16 +38,18 @@ function Details() {
 
   return (
     <main>
-      <article>
+      <article className="details">
         <img
           src={meal.strMealThumb}
           alt=""
         />
-        <div>
-          <h1>{meal.strMeal}</h1>
-          <p>{meal.strCategory}</p>
-          <p>{meal.strArea}</p>
-          <div>
+        <div className="info-card">
+          <h1 className="title">{meal.strMeal}</h1>
+          <div className="tags">
+            <p className="category">{meal.strCategory}</p>
+            <p className="area">{meal.strArea}</p>
+          </div>
+          <div className="navigation">
             {/* Die btn-active Klasse wird hinzugefügt wenn dies die active Seite ist */}
             <button
               onClick={() => setShowInstructions(false)}
@@ -60,72 +63,72 @@ function Details() {
             </button>
           </div>
           {showInstructions || (
-            <div>
+            <div className="ingredients">
               <h2>Ingredients</h2>
               {meal.strIngredient1 && (
-                <p>{`${meal.strMeasure1} ${meal.strIngredient1}`}</p>
+                <p className="ingredient">{`${meal.strMeasure1} ${meal.strIngredient1}`}</p>
               )}
               {meal.strIngredient2 && (
-                <p>{`${meal.strMeasure2} ${meal.strIngredient2}`}</p>
+                <p className="ingredient">{`${meal.strMeasure2} ${meal.strIngredient2}`}</p>
               )}
               {meal.strIngredient3 && (
-                <p>{`${meal.strMeasure3} ${meal.strIngredient3}`}</p>
+                <p className="ingredient">{`${meal.strMeasure3} ${meal.strIngredient3}`}</p>
               )}
               {meal.strIngredient4 && (
-                <p>{`${meal.strMeasure4} ${meal.strIngredient4}`}</p>
+                <p className="ingredient">{`${meal.strMeasure4} ${meal.strIngredient4}`}</p>
               )}
               {meal.strIngredient5 && (
-                <p>{`${meal.strMeasure5} ${meal.strIngredient5}`}</p>
+                <p className="ingredient">{`${meal.strMeasure5} ${meal.strIngredient5}`}</p>
               )}
               {meal.strIngredient6 && (
-                <p>{`${meal.strMeasure6} ${meal.strIngredient6}`}</p>
+                <p className="ingredient">{`${meal.strMeasure6} ${meal.strIngredient6}`}</p>
               )}
               {meal.strIngredient7 && (
-                <p>{`${meal.strMeasure7} ${meal.strIngredient7}`}</p>
+                <p className="ingredient">{`${meal.strMeasure7} ${meal.strIngredient7}`}</p>
               )}
               {meal.strIngredient8 && (
-                <p>{`${meal.strMeasure8} ${meal.strIngredient8}`}</p>
+                <p className="ingredient">{`${meal.strMeasure8} ${meal.strIngredient8}`}</p>
               )}
               {meal.strIngredient9 && (
-                <p>{`${meal.strMeasure9} ${meal.strIngredient9}`}</p>
+                <p className="ingredient">{`${meal.strMeasure9} ${meal.strIngredient9}`}</p>
               )}
               {meal.strIngredient10 && (
-                <p>{`${meal.strMeasure10} ${meal.strIngredient10}`}</p>
+                <p className="ingredient">{`${meal.strMeasure10} ${meal.strIngredient10}`}</p>
               )}
               {meal.strIngredient11 && (
-                <p>{`${meal.strMeasure11} ${meal.strIngredient11}`}</p>
+                <p className="ingredient">{`${meal.strMeasure11} ${meal.strIngredient11}`}</p>
               )}
               {meal.strIngredient12 && (
-                <p>{`${meal.strMeasure12} ${meal.strIngredient12}`}</p>
+                <p className="ingredient">{`${meal.strMeasure12} ${meal.strIngredient12}`}</p>
               )}
               {meal.strIngredient13 && (
-                <p>{`${meal.strMeasure13} ${meal.strIngredient13}`}</p>
+                <p className="ingredient">{`${meal.strMeasure13} ${meal.strIngredient13}`}</p>
               )}
               {meal.strIngredient14 && (
-                <p>{`${meal.strMeasure14} ${meal.strIngredient14}`}</p>
+                <p className="ingredient">{`${meal.strMeasure14} ${meal.strIngredient14}`}</p>
               )}
               {meal.strIngredient15 && (
-                <p>{`${meal.strMeasure15} ${meal.strIngredient15}`}</p>
+                <p className="ingredient">{`${meal.strMeasure15} ${meal.strIngredient15}`}</p>
               )}
               {meal.strIngredient16 && (
-                <p>{`${meal.strMeasure16} ${meal.strIngredient16}`}</p>
+                <p className="ingredient">{`${meal.strMeasure16} ${meal.strIngredient16}`}</p>
               )}
               {meal.strIngredient17 && (
-                <p>{`${meal.strMeasure17} ${meal.strIngredient17}`}</p>
+                <p className="ingredient">{`${meal.strMeasure17} ${meal.strIngredient17}`}</p>
               )}
               {meal.strIngredient18 && (
-                <p>{`${meal.strMeasure18} ${meal.strIngredient18}`}</p>
+                <p className="ingredient">{`${meal.strMeasure18} ${meal.strIngredient18}`}</p>
               )}
               {meal.strIngredient19 && (
-                <p>{`${meal.strMeasure19} ${meal.strIngredient19}`}</p>
+                <p className="ingredient">{`${meal.strMeasure19} ${meal.strIngredient19}`}</p>
               )}
               {meal.strIngredient20 && (
-                <p>{`${meal.strMeasure20} ${meal.strIngredient20}`}</p>
+                <p className="ingredient">{`${meal.strMeasure20} ${meal.strIngredient20}`}</p>
               )}
             </div>
           )}
           {showInstructions && (
-            <div>
+            <div className="instructions">
               <h2>Instructions</h2>
               <p>{meal.strInstructions}</p>
               <a
