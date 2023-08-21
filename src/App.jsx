22e840facf35c5510c8e49a90c8f172d9154d316
Matthew7991 <Matthew7991@gmx.de>
2023-08-21 10:17:44 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
 
 function App() {
-  return <></>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<h1>onboarding</h1>}
+          />
+          <Route
+            path="/home"
+            element={<h1>home</h1>}
+          />
+          <Route
+            path="/search/:searchTerm"
+            element={<h1>search</h1>}
+          />
+          <Route
+            path="/area/:area"
+            element={<h1>search</h1>}
+          />
+          <Route
+            path="/categories/:category"
+            element={<h1>search</h1>}
+          />
+          <Route
+            path="/details/:mealID"
+            element={<h1>details</h1>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
