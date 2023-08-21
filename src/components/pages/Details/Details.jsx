f74a9d743b2b9098e9bdfff2c2fd037254f94b80
Matthew7991 <Matthew7991@gmx.de>
2023-08-21 10:45:23 +0200

    started Details page

--- src/components/pages/Details/Details.jsx
+++ src/components/pages/Details/Details.jsx
@@ -1,7 +1,29 @@
 import React from "react"
 
 function Details() {
-  return <div></div>
+  return (
+    <main>
+      <article>
+        <img
+          src=""
+          alt=""
+        />
+        <div>
+          <h1></h1>
+          <p></p>
+          <p></p>
+          <div>
+            <button>Ingredients</button>
+            <button>Instructions</button>
+          </div>
+          <div>
+            <h2>Ingredients</h2>
+            <p></p>
+          </div>
+        </div>
+      </article>
+    </main>
+  )
 }
 
 export default Details
