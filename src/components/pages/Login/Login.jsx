98a5ddeecc0866c8e73064918bc7e99ab7b4c937
Matthew7991 <Matthew7991@gmx.de>
2023-08-22 12:06:38 +0200

    register input Handler

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,11 +1,48 @@
 import React from "react"
 import Navbar from "../../shared/Navbar/Navbar"
+import UserImg from "../../../assets/Images/Profile.svg"
+import LockImg from "../../../assets/Images/Lock.svg"
+import { Link } from "react-router-dom"
 
 function Login() {
   return (
     <>
       <main>
-        <h1>This is the login page</h1>
+        <article>
+          <form>
+            <h1>Welcome back!</h1>
+            <div>
+              <label htmlFor="username">username</label>
+              <img
+                src={UserImg}
+                alt=""
+              />
+              <input
+                required
+                type="text"
+                id="username"
+                name="username"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">password</label>
+              <img
+                src={LockImg}
+                alt=""
+              />
+              <input
+                required
+                type="password"
+                id="password"
+                name="password"
+              />
+            </div>
+            <button type="submit">Log In</button>
+          </form>
+          <p>
+            Need an account? <Link to={"/register"}>Register</Link>
+          </p>
+        </article>
       </main>
       <Navbar />
     </>
--- src/components/pages/Register/Register.jsx
+++ src/components/pages/Register/Register.jsx
@@ -1,11 +1,101 @@
-import React from "react"
+import React, { useState } from "react"
 import Navbar from "../../shared/Navbar/Navbar"
+import UserImg from "../../../assets/Images/Profile.svg"
+import LockImg from "../../../assets/Images/Lock.svg"
+import MailImg from "../../../assets/Images/Message.svg"
+import { Link } from "react-router-dom"
 
 function Register() {
+  const [inputEmail, setInputEmail] = useState("")
+  const [inputUsername, setInputUsername] = useState("")
+  const [inputPassword, setInputPassword] = useState("")
+  const [inputPasswordConfirm, setInputPasswordConfirm] = useState("")
+
+  const handleSubmit = (event) => {}
+
+  const handleInputEmail = (event) => {
+    setInputEmail(event.target.value)
+  }
+  const handleInputUsername = (event) => {
+    setInputUsername(event.target.value)
+  }
+  const handleInputPassword = (event) => {
+    setInputPassword(event.target.value)
+  }
+  const handleInputPasswordConfirm = (event) => {
+    setInputPasswordConfirm(event.target.value)
+  }
+
   return (
     <>
       <main>
-        <h1>This is the Register page</h1>
+        <article>
+          <form onSubmit={handleSubmit}>
+            <h1>Creat an account</h1>
+            <div>
+              <label htmlFor="email">email</label>
+              <img
+                src={MailImg}
+                alt=""
+              />
+              <input
+                onChange={handleInputEmail}
+                value={inputEmail}
+                required
+                type="email"
+                id="email"
+                name="email"
+              />
+            </div>
+            <div>
+              <label htmlFor="username">username</label>
+              <img
+                src={UserImg}
+                alt=""
+              />
+              <input
+                onChange={handleInputUsername}
+                value={inputUsername}
+                required
+                type="text"
+                id="username"
+                name="username"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">password</label>
+              <img
+                src={LockImg}
+                alt=""
+              />
+              <input
+                onChange={handleInputPassword}
+                value={inputPassword}
+                required
+                type="password"
+                id="password"
+                name="password"
+              />
+            </div>
+            <div>
+              <label htmlFor="password-confirm">Confirm password</label>
+              <img
+                src={LockImg}
+                alt=""
+              />
+              <input
+                onChange={handleInputPasswordConfirm}
+                value={inputPasswordConfirm}
+                required
+                type="password"
+                id="password-confirm"
+                name="password-confirm"
+              />
+            </div>
+            <button type="submit">Register</button>
+          </form>
+          <Link to={"/login"}>Already have an account?</Link>
+        </article>
       </main>
       <Navbar />
     </>
