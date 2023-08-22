4ab6a6754f817b95cdf26043cd419c6fdbf0d8cd
Matthew7991 <Matthew7991@gmx.de>
2023-08-22 16:06:00 +0200

    fixed mergin error

--- src/components/shared/Random/RandomFood.jsx
+++ src/components/shared/Random/RandomFood.jsx
@@ -1,48 +1,49 @@
-
-import React, { useEffect, useState } from "react";
-import { randomApi } from "../../../utilities/randomApi";
-import DetailRandomFood from "../DetailRandomFood/DetailRandomFood";
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react"
+import { randomApi } from "../../../utilities/randomApi"
+import DetailRandomFood from "../DetailRandomFood/DetailRandomFood"
+import { Link } from "react-router-dom"
 import "./RandomFood.css"
 import FoodImg from "../../../assets/Images/Frame.svg"
 
-
 function RandomFood() {
-  const date = new Date();
-  const now = date.getDate().toString() + date.getMonth().toString() + date.getFullYear().toString();
-  
-  const [randomId, setRandomId] = useState("");
-  const [localId, setLocaleId] = useState("");
+  const date = new Date()
+  const now =
+    date.getDate().toString() +
+    date.getMonth().toString() +
+    date.getFullYear().toString()
+
+  const [randomId, setRandomId] = useState("")
+  const [localId, setLocaleId] = useState("")
 
   useEffect(() => {
     fetch(randomApi)
       .then((res) => res.json())
-      .then((data) => setRandomId(data.meals[0].idMeal));
-  }, []);
+      .then((data) => setRandomId(data.meals[0].idMeal))
+  }, [])
 
   useEffect(() => {
     if (randomId) {
       if (localId === null) {
-        setLocaleId(randomId);
-        localStorage.setItem(`${now}`, randomId);
+        setLocaleId(randomId)
+        localStorage.setItem(`${now}`, randomId)
       } else {
-        setLocaleId(localStorage.getItem(`${now}`));
+        setLocaleId(localStorage.getItem(`${now}`))
       }
     }
-  }, [randomId]);
+  }, [randomId])
 
-  if (localId === '') {
+  if (localId === "") {
     return <h1>loading...</h1>
   }
 
   return (
-
     <div>
       <h1>Meal of the Day</h1>
       <DetailRandomFood localId={localId ? localId : randomId} />
     </div>
-  );
-  {/* <div className='random-food'>
+  )
+  {
+    /* <div className='random-food'>
       <h1 className='random-food-title'>Meal of the Day</h1>
       <Link to={`/details/${random.idMeal}`} className='random-food-link'>
         <div className='random-food-img-wraper'>
@@ -54,8 +55,8 @@ function RandomFood() {
             <p className="random-food-area">{random.strArea}</p>
           </div>
           </Link>
-        </div> */}
-  )
+        </div> */
+  }
 }
 
-export default RandomFood;
+export default RandomFood
