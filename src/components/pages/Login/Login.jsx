d390d8949a792a1b9e609a021dbcb1cc9ff23acd
Matthew7991 <Matthew7991@gmx.de>
2023-08-22 15:47:15 +0200

    login, register logic

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,15 +1,60 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Navbar from "../../shared/Navbar/Navbar"
 import UserImg from "../../../assets/Images/Profile.svg"
 import LockImg from "../../../assets/Images/Lock.svg"
 import { Link } from "react-router-dom"
 
 function Login() {
+  const [users, setUsers] = useState(() => {
+    const usersStorage = JSON.parse(localStorage.getItem("users"))
+    return usersStorage ? usersStorage : []
+  })
+
+  const [inputUsername, setInputUsername] = useState("")
+  const [inputPassword, setInputPassword] = useState("")
+  const [loginDataInvalid, setLoginDataInvalid] = useState(false)
+
+  const handleInputUsername = (event) => {
+    setInputUsername(event.target.value)
+  }
+  const handleInputPassword = (event) => {
+    setInputPassword(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log(users)
+
+    if (
+      !users.find(
+        (user) =>
+          (user.username !== inputUsername && user.email !== inputUsername) ||
+          user.password !== inputPassword
+      )
+    ) {
+      setLoginDataInvalid(true)
+      return
+    }
+
+    setUsers(
+      users.map((user) => {
+        return user.username !== inputUsername && user.email !== inputUsername
+          ? { ...user, loggedIn: false }
+          : { ...user, loggedIn: true }
+      })
+    )
+  }
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users))
+  }, [users])
+
   return (
     <>
       <main>
         <article>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h1>Welcome back!</h1>
             <div>
               <label htmlFor="username">username</label>
@@ -18,6 +63,7 @@ function Login() {
                 alt=""
               />
               <input
+                onChange={handleInputUsername}
                 required
                 type="text"
                 id="username"
@@ -31,6 +77,7 @@ function Login() {
                 alt=""
               />
               <input
+                onChange={handleInputPassword}
                 required
                 type="password"
                 id="password"
@@ -38,6 +85,7 @@ function Login() {
               />
             </div>
             <button type="submit">Log In</button>
+            {loginDataInvalid && <output>Login or password invalid</output>}
           </form>
           <p>
             Need an account? <Link to={"/register"}>Register</Link>
