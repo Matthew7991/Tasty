3ad368cc6fd69336507b17c70c86337fb60a6499
Matthew7991 <Matthew7991@gmx.de>
2023-08-22 10:16:19 +0200

    merging

--- src/components/pages/Areas/Areas.jsx
+++ src/components/pages/Areas/Areas.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from "react"
 import SearchBar from "../../shared/SearchBar/SearchBar"
 import "./Areas.scss"
 import FilterList from "../../shared/FilterList/FilterList"
-import { areaFilterApi } from "../../../utilities/areaFilterApi"
 import FoodList from "../../shared/foodList/FoodList"
+import { areaFilterApi } from "../../../utilities/areaFilterApi"
 
 function Areas() {
-  const [areaList, setareaList] = useState([])
   const [area, setArea] = useState("american")
   const [foodList, setFoodList] = useState([])
 
@@ -20,17 +19,12 @@ function Areas() {
       .then((data) => setFoodList(data.meals))
   }, [area])
 
-  useEffect(() => {
-    fetch(areaFilterApi)
-      .then((res) => res.json())
-      .then((data) => setareaList(data.meals))
-  }, [])
-
   return (
     <div className="areas">
       <SearchBar />
       <FilterList
-        dataList={areaList}
+        title="strArea"
+        api={areaFilterApi}
         onHandleFoodList={handleFoodList}
       />
       <FoodList foodList={foodList} />
