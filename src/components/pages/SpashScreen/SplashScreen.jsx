199d9cc7df72ee53973d08b2efbf0ac87040ecc5
FutureOneX <shamilkhangumusel@gmail.com>
2023-08-23 11:09:15 +0200

    Splash screen timer und animation

--- src/components/pages/SpashScreen/SplashScreen.jsx
+++ src/components/pages/SpashScreen/SplashScreen.jsx
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom"
-import Logo from "../../../assets/Images/Logo.svg"
-import "./SplashScreen.css"
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../../../assets/Images/Logo.svg";
+import "./SplashScreen.css";
 
-const SpashScreen = () => {
+const SplashScreen = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/onboarding")
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Link
-      to="/onboarding"
-      className="splash-screen-link">
-      <div className="splash-screen">
-        <img
-          src={Logo}
-          alt="Logo"
-          className="logo"
-        />
-        <h1 className="app-title">Tasty</h1>
-      </div>
-    </Link>
-  )
-}
+    <>
+          <div className="splash-screen">
+            <img src={Logo} alt="Logo" className="logo" />
+            <h1 className="app-title">Tasty</h1>
+          </div>
+    </>
+  );
+};
 
-export default SpashScreen
+export default SplashScreen;
