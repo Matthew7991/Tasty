8f3df807ce3b7bd15f3612acffd24f331d16b26b
Matthew7991 <Matthew7991@gmx.de>
2023-08-24 15:19:32 +0200

    fixed favorite icon in desktop view

--- src/components/pages/Details/Details.jsx
+++ src/components/pages/Details/Details.jsx
@@ -75,46 +75,48 @@ function Details() {
   return (
     <main>
       <article className="details">
-        {currentUser && (
-          <button
-            className={`${
-              currentUser.favorites.find(
-                (favorite) => favorite.idMeal === meal.idMeal
-              ) && "active"
-            }`}
-            onClick={handleToggleFavorite}>
-            <svg
-              width="21"
-              height="20"
-              viewBox="0 0 21 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <g id="Heart">
-                <path
-                  id="Stroke 1"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M1.37187 9.59832C0.298865 6.24832 1.55287 2.41932 5.06987 1.28632C6.91987 0.689322 8.96187 1.04132 10.4999 2.19832C11.9549 1.07332 14.0719 0.693322 15.9199 1.28632C19.4369 2.41932 20.6989 6.24832 19.6269 9.59832C17.9569 14.9083 10.4999 18.9983 10.4999 18.9983C10.4999 18.9983 3.09787 14.9703 1.37187 9.59832Z"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  id="Stroke 3"
-                  d="M14.5 4.7002C15.57 5.0462 16.326 6.0012 16.417 7.1222"
-                  stroke="#C6E3E5"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </g>
-            </svg>
-          </button>
-        )}
-        <img
-          src={meal.strMealThumb}
-          alt=""
-        />
+        <div className="img-wrapper">
+          {currentUser && (
+            <button
+              className={`${
+                currentUser.favorites.find(
+                  (favorite) => favorite.idMeal === meal.idMeal
+                ) && "active"
+              }`}
+              onClick={handleToggleFavorite}>
+              <svg
+                width="21"
+                height="20"
+                viewBox="0 0 21 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <g id="Heart">
+                  <path
+                    id="Stroke 1"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M1.37187 9.59832C0.298865 6.24832 1.55287 2.41932 5.06987 1.28632C6.91987 0.689322 8.96187 1.04132 10.4999 2.19832C11.9549 1.07332 14.0719 0.693322 15.9199 1.28632C19.4369 2.41932 20.6989 6.24832 19.6269 9.59832C17.9569 14.9083 10.4999 18.9983 10.4999 18.9983C10.4999 18.9983 3.09787 14.9703 1.37187 9.59832Z"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    id="Stroke 3"
+                    d="M14.5 4.7002C15.57 5.0462 16.326 6.0012 16.417 7.1222"
+                    stroke="#C6E3E5"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+              </svg>
+            </button>
+          )}
+          <img
+            src={meal.strMealThumb}
+            alt=""
+          />
+        </div>
         <div className="info-card">
           <h1 className="title">{meal.strMeal}</h1>
           <div className="tags">
