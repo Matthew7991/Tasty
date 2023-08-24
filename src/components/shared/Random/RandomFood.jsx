86caae72d0293d344bbd9abaa1c188e9461f3e25
Matthew7991 <Matthew7991@gmx.de>
2023-08-24 10:26:28 +0200

    random food fix

--- src/components/shared/Random/RandomFood.jsx
+++ src/components/shared/Random/RandomFood.jsx
@@ -9,43 +9,32 @@ function RandomFood() {
     date.getDate().toString() +
     date.getMonth().toString() +
     date.getFullYear().toString()
-  localStorage.setItem(`Test${now}`, 642642)
 
-  const [randomId, setRandomId] = useState("")
-  const [localId, setLocaleId] = useState("")
+  const [randomId, setRandomId] = useState(() => {
+    const usersStorage = localStorage.getItem(`${now}`)
+    return usersStorage ? usersStorage : ""
+  })
 
   useEffect(() => {
-    fetch(randomApi)
-      .then((res) => res.json())
-      .then((data) => setRandomId(data.meals[0].idMeal))
+    if (!randomId) {
+      fetch(randomApi)
+        .then((res) => res.json())
+        .then((data) => setRandomId(data.meals[0].idMeal))
+    }
   }, [])
 
   useEffect(() => {
-    console.log({ randomId })
-    if (randomId) {
-      console.log({ localId })
-      console.log("localId null?", localId === null)
-      if (!localId) {
-        localStorage.setItem(`T${now}`, randomId)
-        setLocaleId(randomId)
-      } else {
-        setLocaleId(localStorage.getItem(`T${now}`))
-      }
-    }
+    localStorage.setItem("users", randomId)
   }, [randomId])
 
-  console.log({ localId })
-  console.log("localId oposite", !localId)
-  console.log("localId empty string", localId === "")
-  console.log("localId null", localId === null)
-  if (!localId) {
+  if (!randomId) {
     return <div className="loader"></div>
   }
 
   return (
     <div className="random-food">
       <h1 className="random-food-title">Meal of the Day</h1>
-      <DetailRandomFood localId={localId ? localId : randomId} />
+      <DetailRandomFood localId={randomId} />
     </div>
   )
 }
